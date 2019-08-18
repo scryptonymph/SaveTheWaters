906f6c22e0c76b0f99d53ea5942a80fa710b5ff7
Suvi <suvi.kemppainen@cs.tamk.fi>
2019-08-19 00:45:36 +0300

    ADDED: Icons, pointer finger that goes away when plastic is clicked for the first time

---
+++ Assets/Code/Pointer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pointer : MonoBehaviour
+{
+    [SerializeField] private Plastic _plastic;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (_plastic.CurrentErasedPlastic > 0) {
+            this.gameObject.SetActive(false);
+        }
+    }
+}
